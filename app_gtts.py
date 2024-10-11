50fac02f1686cea883251e093a6d79fda3db8c0a
pazuzu71 <genchez.tula@gmail.com>
2024-10-11 17:28:01 +0300

    Первый коммент

---
+++ app_gtts.py
@@ -0,0 +1,38 @@
+# Import the required module for text
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+import os
+
+# The text that you want to convert to audio
+mytext = '''
+list.append(x)	Добавляет элемент в конец списка
+list.extend(L)	Расширяет список list, добавляя в конец все элементы списка L
+list.insert(i, x)	Вставляет на i-ый элемент значение x
+list.remove(x)	Удаляет первый элемент в списке, имеющий значение x. ValueError, если такого элемента не существует
+list.pop([i])	Удаляет i-ый элемент и возвращает его. Если индекс не указан, удаляется последний элемент
+list.index(x, [start [, end]])	Возвращает положение первого элемента со значением x (при этом поиск ведется от start до end)
+list.count(x)	Возвращает количество элементов со значением x
+list.sort([key=функция])	Сортирует список на основе функции
+list.reverse()	Разворачивает список
+list.copy()	Поверхностная копия списка
+list.clear()	Очищает список
+'''
+
+# Language in which you want to convert
+language = 'ru'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("welcome.mp3")
+
+# Playing the converted file
+os.system("start welcome.mp3")
